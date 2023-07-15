a275f37361fec2339d563125bd1060fe039fbdf4
1D04KSk-sana <C2105@oic.jp>
2023-07-15 23:35:42 +0900

    bussnoyatu

--- lib/start.dart
+++ lib/start.dart
@@ -13,7 +13,7 @@ class StartPage extends StatefulWidget {
 }
 
 class _StartPageState extends State<StartPage> {
-  static const String API_KYE = "AIzaSyB82zSwk2xc4rdlU7Gl2N2HlTtIY_B8xDU";
+  static const String API_KYE = "AIzaSyD4jKa8v1-_3D-jbUYtJv9pqF6BmDv_iHI";
 
   late GoogleMapController mapController;
 
@@ -26,10 +26,19 @@ class _StartPageState extends State<StartPage> {
       const LatLng(34.73302146707623, 135.55713263824782);
 
   int moguleCount = 0;
+  int drawerCount = 0;
   int count = 0;
 
   final _startPoints = [
+    const LatLng(0, 0),
     const LatLng(34.70263531930244, 135.49718441206556),
+    const LatLng(34.68994913458292, 135.51737432883576)
+  ];
+
+  final _destinationPoints = [
+    const LatLng(0, 0),
+    const LatLng(34.73302146707623, 135.55713263824782),
+    const LatLng(34.71653452669397, 135.58266869020815)
   ];
 
   void _onMapCreated(GoogleMapController controller) {
@@ -39,12 +48,11 @@ class _StartPageState extends State<StartPage> {
   @override
   void initState() {
     super.initState();
-    _getRoutes();
   }
 
 // ルート表示データ取得
   Future<void> _getRoutes() async {
-    List<LatLng> _points = await _createPolyline(count);
+    List<LatLng> _points = await _createPolyline();
     setState(() {
       _polyline.add(Polyline(
           polylineId: const PolylineId("Route"),
@@ -76,22 +84,90 @@ class _StartPageState extends State<StartPage> {
                 markerId: const MarkerId("origin"),
                 position: _startPoints[count]),
             Marker(
-                markerId: const MarkerId("destination"), position: destination)
+                markerId: const MarkerId("destination"),
+                position: _destinationPoints[count])
           });
     }
 
     return Maps;
   }
 
+  Widget _createDrawer() {
+    Widget drawers;
+
+    drawers = Drawer(
+      child: ListView(
+        children: [
+          SizedBox(
+              height: 60,
+              child: DrawerHeader(
+                decoration: BoxDecoration(color: Colors.green.withOpacity(0.8)),
+                child: const Text('メニュー'),
+              )),
+          ListTile(
+            title: Text("72番線"),
+            trailing: Icon(Icons.arrow_forward),
+            onTap: () {
+              count = 1;
+              moguleCount = 1;
+              drawerCount = 1;
+              _getRoutes();
+              setState(() {});
+            },
+          ),
+          ListTile(
+            title: Text("46番線"),
+            trailing: Icon(Icons.arrow_forward),
+            onTap: () {
+              count = 2;
+              moguleCount = 1;
+              drawerCount = 1;
+              _getRoutes();
+              setState(() {});
+            },
+          )
+        ],
+      ),
+    );
+
+    if (drawerCount == 1) {
+      drawers = Drawer(
+        child: ListView(
+          children: [
+            SizedBox(
+                height: 60,
+                child: DrawerHeader(
+                  decoration:
+                      BoxDecoration(color: Colors.green.withOpacity(0.8)),
+                  child: const Text('メニュー'),
+                )),
+            ListTile(
+              title: Text("戻る"),
+              trailing: Icon(Icons.arrow_forward),
+              onTap: () {
+                setState(() {
+                  moguleCount = 0;
+                  drawerCount = 0;
+                });
+              },
+            ),
+          ],
+        ),
+      );
+    }
+
+    return drawers;
+  }
+
   // ルート表示
-  Future<List<LatLng>> _createPolyline(int count) async {
+  Future<List<LatLng>> _createPolyline() async {
     List<LatLng> polylineCoordinates = [];
     PolylinePoints polylinePoints = PolylinePoints();
     PolylineResult result = await polylinePoints.getRouteBetweenCoordinates(
       API_KYE,
-      PointLatLng(
-          _startPoints[count - 1].latitude, _startPoints[count - 1].longitude),
-      PointLatLng(destination.latitude, destination.longitude),
+      PointLatLng(_startPoints[count].latitude, _startPoints[count].longitude),
+      PointLatLng(_destinationPoints[count].latitude,
+          _destinationPoints[count].longitude),
       travelMode: TravelMode.walking,
     );
 
@@ -161,33 +237,7 @@ class _StartPageState extends State<StartPage> {
           ))
         ],
       ),
-      drawer: Drawer(
-        child: ListView(
-          children: [
-            SizedBox(
-                height: 60,
-                child: DrawerHeader(
-                  decoration:
-                      BoxDecoration(color: Colors.green.withOpacity(0.8)),
-                  child: const Text('メニュー'),
-                )),
-            ListTile(
-              title: Text("72番線"),
-              trailing: Icon(Icons.arrow_forward),
-              onTap: () {
-                setState(() {
-                  moguleCount = 1;
-                  count = 1;
-                });
-              },
-            ),
-            ListTile(
-              title: Text("Item 2"),
-              trailing: Icon(Icons.arrow_forward),
-            )
-          ],
-        ),
-      ),
+      drawer: _createDrawer(),
     );
   }
 }
