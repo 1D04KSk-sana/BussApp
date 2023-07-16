7b9a31a566bfbbaa572b7d5a7125ae01dad7f3b6
1D04KSk-sana <C2105@oic.jp>
2023-07-16 19:47:40 +0900

    bussnoyatu

--- lib/start.dart
+++ lib/start.dart
@@ -31,14 +31,50 @@ class _StartPageState extends State<StartPage> {
 
   final _startPoints = [
     const LatLng(0, 0),
-    const LatLng(34.70263531930244, 135.49718441206556),
-    const LatLng(34.68994913458292, 135.51737432883576)
+    //10 守口車庫前 1
+    const LatLng(34.68988738252166, 135.51758890555124),
+    //18 玉造 2
+    const LatLng(34.67478085034745, 135.533731508431),
+    //19 地下鉄今里 3
+    const LatLng(34.668373352674244, 135.54330848590925),
+    //21 天満橋 4
+    const LatLng(34.689957956302194, 135.51770692274474),
+    //31 天満橋 5
+    const LatLng(34.689957956302194, 135.51770692274474),
+    //34 守口車庫前 6
+    const LatLng(34.68988738252166, 135.51758890555124),
+    //35 守口車庫前 7
+    const LatLng(34.68988738252166, 135.51758890555124),
+    //45 総合医療センター前 8
+    const LatLng(34.708065656346754, 135.52383196692364),
+    //46 天満橋 9
+    const LatLng(34.689957956302194, 135.51770692274474),
+    //78 守口車庫前 10
+    const LatLng(34.68988738252166, 135.51758890555124),
   ];
 
   final _destinationPoints = [
     const LatLng(0, 0),
-    const LatLng(34.73302146707623, 135.55713263824782),
-    const LatLng(34.71653452669397, 135.58266869020815)
+    //10 大阪駅前 1
+    const LatLng(34.70224785277654, 135.49733589336918),
+    //18 北巽バスターミナル 2
+    const LatLng(34.65360888639565, 135.55561117486508),
+    //19 加美東三丁目北 3
+    const LatLng(34.63264141232815, 135.5737847920003),
+    //21 地下鉄深江橋 4
+    const LatLng(34.67834578367938, 135.55864897247486),
+    //31 花博記念公園北口 5
+    const LatLng(34.71623994599965, 135.56603133993826),
+    //34 大阪駅前 6
+    const LatLng(34.70224785277654, 135.49733589336918),
+    //35 杭全 7
+    const LatLng(34.63961862309295, 135.53857859385073),
+    //45 諸口 8
+    const LatLng(34.70457140252267, 135.57883300555125),
+    //46 焼野 9
+    const LatLng(34.713767572663386, 135.5824950220603),
+    //78 大阪駅前 10
+    const LatLng(34.70224785277654, 135.49733589336918),
   ];
 
   void _onMapCreated(GoogleMapController controller) {
@@ -52,7 +88,8 @@ class _StartPageState extends State<StartPage> {
 
 // ルート表示データ取得
   Future<void> _getRoutes() async {
-    List<LatLng> _points = await _createPolyline();
+    List<LatLng> _points = [];
+    _points = await _createPolyline();
     setState(() {
       _polyline.add(Polyline(
           polylineId: const PolylineId("Route"),
@@ -92,6 +129,7 @@ class _StartPageState extends State<StartPage> {
     return Maps;
   }
 
+  //ドロワーの作成
   Widget _createDrawer() {
     Widget drawers;
 
@@ -105,7 +143,7 @@ class _StartPageState extends State<StartPage> {
                 child: const Text('メニュー'),
               )),
           ListTile(
-            title: Text("72番線"),
+            title: Text("10番線"),
             trailing: Icon(Icons.arrow_forward),
             onTap: () {
               count = 1;
@@ -116,10 +154,98 @@ class _StartPageState extends State<StartPage> {
             },
           ),
           ListTile(
-            title: Text("46番線"),
+            title: Text("18番線"),
             trailing: Icon(Icons.arrow_forward),
             onTap: () {
               count = 2;
+              moguleCount = 1;
+              drawerCount = 1;
+              _getRoutes();
+              setState(() {});
+            },
+          ),
+          ListTile(
+            title: Text("19番線"),
+            trailing: Icon(Icons.arrow_forward),
+            onTap: () {
+              count = 3;
+              moguleCount = 1;
+              drawerCount = 1;
+              _getRoutes();
+              setState(() {});
+            },
+          ),
+          ListTile(
+            title: Text("21番線"),
+            trailing: Icon(Icons.arrow_forward),
+            onTap: () {
+              count = 4;
+              moguleCount = 1;
+              drawerCount = 1;
+              _getRoutes();
+              setState(() {});
+            },
+          ),
+          ListTile(
+            title: Text("31番線"),
+            trailing: Icon(Icons.arrow_forward),
+            onTap: () {
+              count = 5;
+              moguleCount = 1;
+              drawerCount = 1;
+              _getRoutes();
+              setState(() {});
+            },
+          ),
+          ListTile(
+            title: Text("34番線"),
+            trailing: Icon(Icons.arrow_forward),
+            onTap: () {
+              count = 6;
+              moguleCount = 1;
+              drawerCount = 1;
+              _getRoutes();
+              setState(() {});
+            },
+          ),
+          ListTile(
+            title: Text("35番線"),
+            trailing: Icon(Icons.arrow_forward),
+            onTap: () {
+              count = 7;
+              moguleCount = 1;
+              drawerCount = 1;
+              _getRoutes();
+              setState(() {});
+            },
+          ),
+          ListTile(
+            title: Text("45番線"),
+            trailing: Icon(Icons.arrow_forward),
+            onTap: () {
+              count = 8;
+              moguleCount = 1;
+              drawerCount = 1;
+              _getRoutes();
+              setState(() {});
+            },
+          ),
+          ListTile(
+            title: Text("46番線"),
+            trailing: Icon(Icons.arrow_forward),
+            onTap: () {
+              count = 9;
+              moguleCount = 1;
+              drawerCount = 1;
+              _getRoutes();
+              setState(() {});
+            },
+          ),
+          ListTile(
+            title: Text("78番線"),
+            trailing: Icon(Icons.arrow_forward),
+            onTap: () {
+              count = 10;
               moguleCount = 1;
               drawerCount = 1;
               _getRoutes();
